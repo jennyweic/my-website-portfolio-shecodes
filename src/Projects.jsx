622cf5c68c09c442de909ea349a1e723dc2e302d
JenniferC <weicchan@gmail.com>
2024-10-26 08:10:56 +0100

    fix pdf

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import App from './App';
 import './Projects.css';
-import Video from '../src/assets/Jwei-Recipes.mov';
-import Video2 from '../src/assets/Fitness.mov';
-import Video3 from '../src/assets/Weather.mov';
-import Video4 from '../src/assets/World-Clock.mov';
 
 export default function Projects (){
     return(
@@ -23,7 +18,7 @@ export default function Projects (){
      <div className="item">
         <a href="https://wwww.tubular-biscotti-9cd7dd.netlify.app"><h2>Jwei Recipes</h2></a>
        <div><video controls>
-            <source src={Video} type="video/mov"alt="Jwei-recipes-app"/>
+            <source src="assets/Jwei-Recipes.mov" type="video/mov"alt="Jwei-recipes-app"/>
             </video></div> 
         <p>New recipes collection platform. Stay tuned for more updates.</p>
       </div>
@@ -31,7 +26,7 @@ export default function Projects (){
         <a href="https://dashing-concha-c74694.netlify.app"><h2>Fitness</h2></a>
 <div>
     <video controls>
-        <source src={Video2} type="video/mov" alt="Fitness-website"/>
+        <source src="/assets/Fitness.mov" type="video/mov" alt="Fitness-website"/>
     </video>
 </div>
         <p>A hobby blog site focuses on fitness improves wellness and love the toggle theme feature.</p>
@@ -41,7 +36,7 @@ export default function Projects (){
 
         <div>
     <video controls>
-        <source src={Video3} type="video/mov" alt="Weather-app"/>
+        <source src="/assets/Weather.mov" type="video/mov" alt="Weather-app"/>
     </video>
 </div>
         <p>Weather app helps to find your daily and six day forecast weather information.</p>
@@ -51,7 +46,7 @@ export default function Projects (){
         <a href="https://www.jennifers-world-clock.netlify.app"><h2>World Clock</h2></a>
         <div>
     <video controls>
-        <source src={Video4} type="video/mov" alt="World-clock"/>
+        <source src="/assets/World-Clock.mov" type="video/mov" alt="World-clock"/>
     </video>
 </div>
         <p>Select a (local) city for the latest date and time to find your locations real time clock information.</p>
