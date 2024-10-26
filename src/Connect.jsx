8ff59a29c3867866bdec7527a6b5e6d6ccf27ede
JenniferC <weicchan@gmail.com>
2024-10-26 07:27:19 +0100

    Updated connection links

--- src/Connect.jsx
+++ src/Connect.jsx
@@ -8,10 +8,17 @@ export default function Connect (){
 <h3>Connect</h3>
 <p>Connect and follow me on this journey.</p>
 <div className="Connect-items">
-    <button className="button-contacts">Linkedin</button>
-    <button className="button-contacts">GitHub</button>    
-    <button className="button-contacts">X</button>   
-    <button className="button-contacts">Contact</button>  
+    <button className="button-contacts"> 
+        <a href="https://www.linkedin.com/in/chanwjennifer/">LinkedIn</a>
+    </button>
+    <button className="button-contacts">
+        <a href="https://github.com/jennyweic">GitHub</a>
+        </button>    
+    <button className="button-contacts">
+    <a href="https://x.com/weicchan">X</button>   
+    <button className="button-contacts">
+        <a href="" target="_blank">Read My CV</a>
+    </button>  
 </div>    
 </div>)
 }
